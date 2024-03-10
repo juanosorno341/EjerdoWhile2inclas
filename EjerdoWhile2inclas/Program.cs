f3621da012e520e3c4d8a8ed22d180192268cebf
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-03-10 04:23:15 -0500

    Agregar archivos de proyecto.

---
+++ EjerdoWhile2inclas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerdoWhile2inclas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int acum = 0;
+            int i = 0;
+            
+
+                do
+                {
+                    
+                    acum += i;
+                    i++;
+                    
+                    
+                }
+                while (i<= 100);
+
+                {
+                    Console.WriteLine(acum);
+                }
+                Console.ReadKey();
+            
+        }
+    }
+}
